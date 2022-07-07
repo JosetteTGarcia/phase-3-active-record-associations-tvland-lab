1dcc7561bf66551ed14cc496c93419edcc4b49da
Josette Garcia <josettetgarcia@gmail.com>
2022-07-06 22:37:29 -0500

    commit tree 8998f0c44f2f8afd17a3372b71e684ac86cf08a0

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    catchphease = self.catchphrase
+    name = self.name
+    "#{name} always says: #{catchphease}"
+  end
+
 end
